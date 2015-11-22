f3509ed6c0d232303cb218e24166acd686d60bb6
Valdemir Filho <valdemirfilho@alu.ufc.br>
2015-11-22 15:47:53 -0300

    Coding Dojo - 15-10-2015

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,9 +1,9 @@
 def FizzBuzz(number):
-        ret=""
-        if number%3==0:
-            ret+="fizz"
-        if number%5==0:
-            ret+= "buzz"
-        if ret=="":
+        ret = ""
+        if number % 3 == 0:
+            ret += "fizz"
+        if number % 5 == 0:
+            ret += "buzz"
+        if ret == "":
             ret=int(number)
         return ret
---
+++ fizzbuzz/fizzbuzz_test.py
@@ -0,0 +1,24 @@
+import unittest
+from fizzbuzz import FizzBuzz
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_FizzBuzz_recebe_numero_simples(self):
+        self.assertEqual(FizzBuzz(1), 1)
+        self.assertEqual(FizzBuzz(2), 2)
+        self.assertEqual(FizzBuzz(4), 4)
+
+    def teste_Multiplo_de_3(self):
+        self.assertEqual(FizzBuzz(3), "fizz")
+
+
+    def teste_FizzBuzz_de_5(self):
+        self.assertEqual(FizzBuzz(5), "buzz")
+        self.assertEqual(FizzBuzz(10),"buzz")
+
+
+    def teste_FizzBuzz_de_3_e_5(self):
+        self.assertEqual(FizzBuzz(30),"fizzbuzz" )
+
+
+if __name__ == '__main__':
+    unittest.main()
